548ef1e608ed001c179215def4d2fd2921cd85d4
Emerson Dove <52636744+EmersonDove@users.noreply.github.com>
2022-08-12 11:27:18 -0400

    Move to using cv2

--- shadebags/compression/image_formats.py
+++ shadebags/compression/image_formats.py
@@ -16,10 +16,11 @@ class PNG:
 
     @staticmethod
     def decompress(format_, width, height, image: bytes) -> bytes:
-        buf = io.BytesIO()
-        Image.frombytes(format_, (width, height), image, 'PNG').save(buf, format='raw')
-        png_bytes = buf.getvalue()
-        return png_bytes
+        input_buf = io.BytesIO(image)
+        output_buf = io.BytesIO()
+        Image.open(input_buf, formats=('png',)).save(output_buf, format_='raw')
+        raw_bytes = output_buf.getvalue()
+        return raw_bytes
 
 
 class JPEG:
@@ -35,7 +36,8 @@ class JPEG:
 
     @staticmethod
     def decompress(format_, width, height, image: bytes) -> bytes:
-        buf = io.BytesIO()
-        Image.frombytes(format_, (width, height), image, 'JPEG').save(buf, format='raw')
-        jpeg_bytes = buf.getvalue()
-        return jpeg_bytes
+        input_buf = io.BytesIO(image)
+        output_buf = io.BytesIO()
+        Image.open(input_buf, formats=('jpeg',)).save(output_buf,  format_='raw')
+        raw_bytes = output_buf.getvalue()
+        return raw_bytes
